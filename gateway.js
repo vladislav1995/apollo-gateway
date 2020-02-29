420a54fd886a7d979c5f4885b35c49c92c762cfc
Vladislav Pliasov <42614097+vladislav1995@users.noreply.github.com>
2020-02-29 10:55:58 +0300

    Add files via upload

---
+++ gateway.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
+
+function initGateway() {
+  let services = [
+    { name: 'user', url: `http://localhost:7070/graphql/user`},
+    { name: 'film', url: `http://localhost:8080/graphql/film`},
+    { name: 'review', url: `http://localhost:9090/graphql/review`},
+  ];
+
+  console.log('initGateway with services:', services);
+
+  return new ApolloGateway({
+    serviceList: services,
+    buildService: ({ name, url}) => (new RemoteGraphQLDataSource({ name, url })),
+  });
+}
+
+let gateway = initGateway();
+
+(async () => {
+  const { schema, executor } = await gateway.load();
+
+
+  const server = new ApolloServer({
+    schema,
+    executor,
+    subscriptions: false,
+  });
+
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  const PORT = 4000;
+  app.listen(PORT, () => console.log(`ð<9f><9a><80> Server ready at http://localhost:${PORT}/`));
+})().catch(
+    e => {
+      console.error(e)
+    });
---
+++ start.sh
@@ -0,0 +1,4 @@
+npm install express apollo-express @apollo/gateway
+
+# run server
+node --max_old_space_size=1024 gateway.js
